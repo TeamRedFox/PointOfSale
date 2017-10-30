ebca1077cbe8da136efc267e167804626cbd051f
xian14 <alconj9@students.rowan.edu>
2017-10-29 22:33:10 -0400

    Create InvalidUsernameOrPasswordException.java

---
+++ src/model/InvalidUsernameOrPasswordException.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+//check this exception - are inputs okay?
+//should inputs be variables?
+
+//mostly complete as of 4/20/17
+public class InvalidUsernameOrPasswordException extends RuntimeException{
+
+	private String invalidField;
+	private LocalTime timeThrown;
+
+	public InvalidUsernameOrPasswordException(String invalidField) {
+		this.invalidField = invalidField;
+		this.timeThrown = LocalTime.now();
+	}
+
+	public String getErrorMessage() {
+		String message;
+		if (invalidField.equals("username")) {
+			message = "The inputted username does not match any in our system.";
+		}
+		else if (invalidField.equals("password")) {
+			message = "Incorrect password entered.";
+		}
+		else {
+			message = "One or more login fields is empty.";
+		}
+		
+		return message + " " + LocalDate.now() + " " + LocalTime.now(); // adds timestamp when error  occured, remove if needed
+	}
+	
+}
