9d35c39ad6d8d1407b059aaf2414f8456ba459ed
xian14 <alconj9@students.rowan.edu>
2017-10-29 22:34:31 -0400

    Create LoginPanel.java

---
+++ src/view/LoginPanel.java
@@ -0,0 +1,109 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+
+//check to see if all methods are needed
+//as of now (4/20/17) complete - just need to center textfields for login
+public class LoginPanel extends JPanel {
+
+	//component variables
+	JTextField usernameTxt = new JTextField(10);
+	JPasswordField passwordTxt = new JPasswordField(10);
+	JButton signInBtn = new JButton("Sign In");
+	JLabel errorLbl = new JLabel();
+	
+	private static String message = "\nWelcome to TeamRedFox's POS System \n Enter your user credentials below!";
+	private static final int START_ROWS = 5;
+	private static final int START_COLUMNS = 1;
+	
+	public LoginPanel() {
+		JTextPane welcome = new JTextPane();
+		welcome.setEditable(false);
+		welcome.setText(message);
+		welcome.setBackground(this.getBackground());
+		welcome.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		
+		//inspired by http://stackoverflow.com/questions/3213045/centering-text-in-a-jtextarea-or-jtextpane-horizontal-text-alignment
+		//Centering welcome text
+		StyledDocument doc = welcome.getStyledDocument();
+		SimpleAttributeSet center = new SimpleAttributeSet();
+		StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+		doc.setParagraphAttributes(0, doc.getLength(), center, false);
+		
+		this.setLayout(new BorderLayout());
+		this.add(welcome, BorderLayout.NORTH);
+		
+		JPanel infoPanel = new JPanel();
+		infoPanel.setLayout(new GridLayout(START_ROWS, START_COLUMNS));
+
+		JPanel userPnl = new JPanel();
+		JLabel userLbl = new JLabel("Username");
+		userPnl.setLayout(new FlowLayout());
+		userPnl.add(userLbl);
+		userPnl.add(usernameTxt);
+		infoPanel.add(userPnl);
+		
+		JPanel passPnl = new JPanel();
+		JLabel passLbl = new JLabel("Password");
+		passPnl.setLayout(new FlowLayout());
+		passPnl.add(passLbl);
+		passPnl.add(passwordTxt);
+		infoPanel.add(passPnl);
+		
+		JPanel signInBtnPnl = new JPanel();
+		signInBtnPnl.add(signInBtn);
+		infoPanel.add(errorLbl);
+		infoPanel.add(signInBtnPnl);
+		
+		infoPanel.add(new JPanel());
+		
+		this.add(infoPanel, BorderLayout.SOUTH);
+	
+	}
+
+	public JTextField getUsernameTxt() {
+		return usernameTxt;
+	}
+
+	public void setUsernameTxt(JTextField usernameTxt) {
+		this.usernameTxt = usernameTxt;
+	}
+
+	public JPasswordField getPasswordTxt() {
+		return passwordTxt;
+	}
+
+	public void setPasswordTxt(JPasswordField passwordTxt) {
+		this.passwordTxt = passwordTxt;
+	}
+
+	public JButton getSignInBtn() {
+		return signInBtn;
+	}
+
+	public void setSignInBtn(JButton signInBtn) {
+		this.signInBtn = signInBtn;
+	}
+
+	public JLabel getErrorLbl() {
+		return errorLbl;
+	}
+
+	public void setErrorLbl(JLabel errorLbl) {
+		this.errorLbl = errorLbl;
+	}
+		
+}
