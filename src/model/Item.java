072d1ce1d845afd04b7fcf91dc550f419b20c25a
xian14 <alconj9@students.rowan.edu>
2017-10-29 22:33:28 -0400

    Create Item.java

---
+++ src/model/Item.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Item {
+
+	private String itemName;
+	private String itemDesc;
+	private int quantity;
+	private int barcode;
+	
+	public Item(){
+		this.itemName = "";
+		this.itemDesc = "";
+		this.quantity = 0;
+		this.barcode = 0;
+	}
+}
