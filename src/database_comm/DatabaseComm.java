4b1a0aec0e5522940cd4d0c1f3a9082c9029d8b4
Brian Intile <brianintile@gmail.com>
2017-11-04 22:01:19 -0400

    Implement getting user from login

--- src/database_comm/DatabaseComm.java
+++ src/database_comm/DatabaseComm.java
@@ -1,6 +1,5 @@
 package database_comm;
 import java.sql.*;
-import com.microsoft.sqlserver.jdbc.*;
 
 public class DatabaseComm
 {
@@ -31,11 +30,32 @@ public class DatabaseComm
 		}
 	}
 
+	//Debug user class for SQL connection
+	//TODO remove
+	private static class User
+	{
+		public String username;
+		public String passwordHash;
+		public String firstName;
+		public String lastName;
+		
+		public String toString()
+		{
+			return username + ", " + passwordHash + ", " + firstName + " " + lastName;
+		}
+	}
+
+	//Debug item class for SQL connection
+	//TODO remove
+
 	//TODO make not main
 	public static void main(String[] args) throws ClassNotFoundException, SQLException
 	{
 		Item retrievedItem = getItemFromBarcode("12345678");
 		System.out.println(retrievedItem);
+		
+		User retrievedUser = getUserFromLogin("bintile", "123");
+		System.out.println(retrievedUser);
 	}
 	
 	public static Item getItemFromBarcode(String barcode) throws ClassNotFoundException, SQLException
@@ -50,7 +70,7 @@ public class DatabaseComm
 			//Create statement
 			Statement stmt = con.createStatement();
 
-			//Create and execute query to find items
+			//Create and execute query to find item with matching barcode
 			String query = "SELECT * FROM ITEMS WHERE BARCODE = '" + barcode + "'";
 			ResultSet rs = stmt.executeQuery(query);
 			
@@ -69,7 +89,44 @@ public class DatabaseComm
 				//If not, return null
 				System.out.println("Item with barcode " + barcode + " not found");
 				return null;
-			}	
-
+			}
 	}
+	
+	public static User getUserFromLogin(String username, String passwordHash) throws ClassNotFoundException, SQLException
+	{
+
+		//I don't know what this line really does
+		//No touch
+		Class.forName(JDBC_DRIVER);
+
+		System.out.println("Connecting to database...");
+		Connection con = DriverManager.getConnection(connectionUrl);
+
+		//Create statement
+		Statement stmt = con.createStatement();
+
+		//Create and execute query to find item with matching name and password hash
+		String query = "SELECT * FROM USERS WHERE USERNAME = '" + username + "' AND PASS_HASH = '" +  passwordHash + "'";
+		ResultSet rs = stmt.executeQuery(query);
+		
+		//Check if our query got any results
+		if (rs.next())
+		{
+			//If so, return item with variables assigned
+			User returnUser = new User();
+			returnUser.username = rs.getString("USERNAME");
+			returnUser.passwordHash = rs.getString("PASS_HASH");
+			returnUser.firstName = rs.getString("FIRST");
+			returnUser.lastName = rs.getString("LAST");
+			return returnUser;
+		}
+		else
+		{
+			//If not, return null
+			System.out.println("User with the given credentials not found");
+			return null;
+		}
+	}
+	
+	
 }
