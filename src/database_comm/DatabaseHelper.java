9d0157a31b06895b65e7ba989be56a3febf06692
Brian Intile <brianintile@gmail.com>
2017-11-30 17:59:37 -0500

    Tie DatabaseConnection to config file

--- src/database_comm/DatabaseHelper.java
+++ src/database_comm/DatabaseHelper.java
@@ -5,6 +5,10 @@ public class DatabaseHelper
 	/**Formats and sanitizes a string (varchar) value to be put into the database without errors*/
 	public static String formatStringField(String input)
 	{
+		//Make string empty if null
+		if (input == null)
+			input = "";
+		
 		//Single quotes are interpreted as quotations in SQL, replace them with their literal (double quotes)
 		input = input.replace("'", "''");
 		
