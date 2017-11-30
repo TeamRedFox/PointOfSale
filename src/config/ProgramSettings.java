64a987af03a3b07e2e72c851ca5943061f2d7e14
Brian Intile <brianintile@gmail.com>
2017-11-30 17:24:50 -0500

    Rename UserSettings to ProgramSettings

--- src/config/ProgramSettings.java
+++ src/config/ProgramSettings.java
@@ -8,7 +8,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
 
-public class UserSettings
+public class ProgramSettings
 {
 	private final static String configFilePath = "config.properties";
 
