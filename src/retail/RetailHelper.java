a4b51049389b66734886607f5877dfdae2a65e2d
Brian Intile <brianintile@gmail.com>
2017-12-14 00:53:06 -0500

    Add getRegisterItemString to RetailHelper

--- src/retail/RetailHelper.java
+++ src/retail/RetailHelper.java
@@ -18,4 +18,32 @@ public class RetailHelper
 		String[] parts = cashString.split(".");
 		return (Integer.parseInt(parts[0]) * 100) + Integer.parseInt(parts[1]);
 	}
+
+	private static final int maxBarcodeLength = 15;
+	private static final int maxDescriptionLength = 30;
+	private static final int maxPriceLength = 10;
+	
+	/**Returns a string with barcode, price, and description with even spacing, and "..." as needed*/
+	public static String getRegisterItemString(Item item)
+	{
+		return forceStringLength(item.getBarcode(), maxBarcodeLength)
+				+ forceStringLength(item.getDescription(), maxDescriptionLength)
+				+ "$" + item.getPriceString();
+	}
+	
+	private static String forceStringLength(String string, int fitLength)
+	{
+		if (string.length() > fitLength - 1)
+		{
+			string = string.substring(0, fitLength - 4);
+			string += "...";
+		}
+		
+		while (string.length() < fitLength)
+		{
+			string += " ";
+		}
+		
+		return string;
+	}
 }
